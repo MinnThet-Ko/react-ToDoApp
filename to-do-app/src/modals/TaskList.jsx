e926ee91a3022c97c3a86568c5610925c90cb5b1
MinnThet-Ko <blltspd@gmail.com>
2024-01-04 11:47:36 +0630

    Added style to the TaskListCard

--- to-do-app/src/modals/TaskList.jsx
+++ to-do-app/src/modals/TaskList.jsx
@@ -1,7 +1,16 @@
+
+
 function TaskList(){
     let title = "Urgent tasks";
+    const taskListStyle = {
+        height: 300+"px",
+        width: 600+"px",
+        border: "1px solid black",
+        borderRadius: "5%",
+        boxShadow: "grey 10px 10px"
+    }
     return (
-        <div>
+        <div style={taskListStyle} className="taskListCard">
             <h1>{title}</h1>
         </div>
     );
