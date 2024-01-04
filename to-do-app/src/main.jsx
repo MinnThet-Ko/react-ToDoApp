5178b423c1bb55501392aa2dd65d65dbad7fb01e
MinnThet-Ko <blltspd@gmail.com>
2024-01-04 11:29:17 +0630

    Demo React Project

---
+++ to-do-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TaskList from './modals/TaskList'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TaskList />
+  </React.StrictMode>,
+)
---
+++ to-do-app/src/modals/TaskList.jsx
@@ -0,0 +1,10 @@
+function TaskList(){
+    let title = "Urgent tasks";
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    );
+}
+
+export default TaskList;
